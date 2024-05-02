d88f35496fd3209cbdb289838d64dcea446f66d0
Anvitha Goli <anvithagoli@Anvithas-MBP.lan>
2024-05-02 00:14:32 -0700

    small fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import { FunFactCard } from './components/funfactcard/funfactcard';
+import { HomePage } from './components/homepage/homepage';
+import { About } from './components/about/about';
 
 function App() {
   return (
